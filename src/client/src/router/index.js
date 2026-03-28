5bd44a4f23cea7fd8bc9180ee7ecaf94c29b1765
THercules <thhercules2012@gmail.com>
2026-03-28 16:18:36 -0300

    chore: remove unused components, adjust gitignore and adjust routes

--- src/client/src/router/index.js
+++ src/client/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import HomeView from '../views/HomeView.vue'
 import LoginRegister from '@/views/LoginRegister.vue'
 import ScheduleConsultationView from '@/views/ScheduleConsultationView.vue'
 import UserDashboard from '@/views/UserDashboard.vue'
@@ -8,18 +7,6 @@ import UserDashboard from '@/views/UserDashboard.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      meta: { layout: 'default' }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-      meta: { layout: 'default' }
-    },
     {
       path: '/login-register', 
       name: 'login-register',
